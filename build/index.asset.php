f96e2f28b4c2753513bb00eedc31790abe7084e3
Katya Leurdo <edzakharova@yandex.ru>
2021-03-06 13:12:50 +0300

    Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'dependencies' => array(
+		'wp-block-editor',
+		'wp-blocks',
+		'wp-element',
+		'wp-i18n',
+		'wp-polyfill',
+	),
+	'version' => '8bafce8fe70060d1cde8175750cf42bd',
+);
